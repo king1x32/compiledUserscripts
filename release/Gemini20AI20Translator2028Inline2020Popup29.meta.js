7d47632a60015650c829acc48c1ff752c1a0ab6a
king1x32 <king1x32@users.noreply.github.com>
2025-03-15 07:10:34 +0000

    release

---
+++ release/Gemini20AI20Translator2028Inline2020Popup29.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Gemini AI Translator (Inline & Popup)
+// @namespace    Gemini AI Translator (Inline & Popup)
+// @version      4.2
+// @author       King1x32
+// @icon         https://raw.githubusercontent.com/king1x32/UserScripts/refs/heads/main/kings.jpg
+// @match        *://*/*
+// @match        file:///*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @connect      api.gemini.com
+// @require      https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js
+// @homepageURL  https://github.com/king1x32/UserScripts
+// @downloadURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Gemini20AI20Translator2028Inline2020Popup29.user.js
+// @updateURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Gemini20AI20Translator2028Inline2020Popup29.meta.js
+// ==/UserScript==
---
+++ release/README.md
@@ -0,0 +1,3 @@
+### [:house:](https://greasyfork.org/vi/scripts/529348-gemini-ai-translator-inline-popup?locale_override=1) - [Gemini AI Translator (Inline & Popup)](https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Gemini20AI20Translator2028Inline2020Popup29.user.js)
+
+### [:house:](https://greasyfork.org/vi/scripts/529349-prev-next-page-on-screen?locale_override=1) - [Prev Next Page on Screen (Voz)](https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Prev20Next20Page20on20Screen.user.js)
