967907272a18bb8d67c0456c7bca9f11813ca7c3
king1x32 <king1x32@users.noreply.github.com>
2025-07-26 07:15:46 +0000

    release

---
+++ release/Smoothscroll.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Smoothscroll
+// @author       Creec Winceptor
+// @namespace https://greasyfork.org/users/3167
+// @include     *
+// @version 12
+// @downloadURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Smoothscroll.user.js
+// @updateURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Smoothscroll.meta.js
+// ==/UserScript==
