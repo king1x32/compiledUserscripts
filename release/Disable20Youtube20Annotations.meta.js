545e92534c72b973481a6cac17f1558302ebf7da
king1x32 <king1x32@users.noreply.github.com>
2025-08-25 07:16:34 +0000

    release

---
+++ release/Disable20Youtube20Annotations.meta.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     Disable Youtube Annotations
+// @version  1
+// @match    https://www.youtube.com/*
+// @namespace https://greasyfork.org/users/396494
+// @downloadURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Disable20Youtube20Annotations.user.js
+// @updateURL https://raw.githubusercontent.com/king1x32/compiledUserscripts/release/release/Disable20Youtube20Annotations.meta.js
+// ==/UserScript==
